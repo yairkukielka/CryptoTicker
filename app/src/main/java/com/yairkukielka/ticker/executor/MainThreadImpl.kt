5aab5582886ca0942462918e9f945cc8782cc4ff
Yair Kukielka <yair.kukielka@gmail.com>
2019-10-29 10:48:50 +0100

    fixed warning

--- app/src/main/java/com/yairkukielka/ticker/executor/MainThreadImpl.kt
+++ app/src/main/java/com/yairkukielka/ticker/executor/MainThreadImpl.kt
@@ -5,11 +5,7 @@ import android.os.Looper
 
 class MainThreadImpl : MainThread {
 
-    private val handler: Handler
-
-    init {
-        this.handler = Handler(Looper.getMainLooper())
-    }
+    private val handler: Handler = Handler(Looper.getMainLooper())
 
     override fun post(runnable: Runnable) {
         handler.post(runnable)
